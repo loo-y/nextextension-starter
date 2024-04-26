0059b99c86202f53f1bd9ff566e4b1926be0398c
loo-y <loo-y@outlook.com>
2024-04-26 16:33:53 +0800

    Create service-worker.js

---
+++ extension/service-worker.js
@@ -0,0 +1,30 @@
+const Match_URL = ['.com']
+
+const mainPage = './dist/main.html'
+const optionsPage = './dist/options.html'
+
+chrome.action.onClicked.addListener(async tab => {
+    console.log(`chrome action onClicked`)
+    if (!tab.url) return
+    const url = new URL(tab.url)
+    const tabId = tab.id
+    const isInMatchUrl = Match_URL.some(function (matchurl) {
+        return url.origin.includes(matchurl)
+    })
+
+    if (isInMatchUrl) {
+        // inject script in page first
+    }
+})
+
+// hook by background service-worker
+chrome.scripting.registerContentScripts([
+    {
+        id: 'subenv-extension-inject',
+        matches: ['http://*/*', 'https://*/*'],
+        js: ['inject-script.js'],
+        runAt: 'document_start',
+        world: 'MAIN',
+        allFrames: true,
+    },
+])
